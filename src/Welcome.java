c4000b22acf799b945073fbcd51a4f89206c7592
TruongNguyenHub <20074391.truong@student.iuh.edu.vn>
2024-08-16 12:43:06 +0700

    init project

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyễn Thanh Quan Trường!");
+        System.out.println("Welcome to the DHHTTT19A!");
+    }
+}
